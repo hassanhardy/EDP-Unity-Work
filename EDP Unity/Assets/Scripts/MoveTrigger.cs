80fda9de0be7152c0fbf3c225ec9307e82fd2200
ktsuench <will289@hotmail.com>
2019-03-22 18:26:34 -0400

    added in initial use of arkit.dll

--- EDP Unity/Assets/Scripts/MoveTrigger.cs
+++ EDP Unity/Assets/Scripts/MoveTrigger.cs
@@ -2,37 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTrigger : MonoBehaviour
+  public class MoveTrigger : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public float turnSpeed = 50f;
+  public float moveSpeed = 10f;
+  public float turnSpeed = 50f;
 
 
-    void Update()
+  void Update()
+  {
+    /*if (Input.GetKey(KeyCode.Alpha1))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha2))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha3))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha4))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha5))
+        transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha6))
+        transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha7))
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.Alpha8))
+        transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);*/
+
+    if (WebCamera.fp != null && WebCamera.ip != null)
     {
-        if (Input.GetKey(KeyCode.Alpha1))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+      print("camera is calibrated " + WebCamera.ip.IsCalibrated);
+      print("feature point inlier ratio " + WebCamera.fp.InlierRatio);
+      WebCamera.fp.GetPose(WebCamera.ip.CameraMatrix, WebCamera.ip.DistortionCoefficients, out Emgu.CV.Mat rotationMat, out Emgu.CV.Mat translationVector);
 
-        if (Input.GetKey(KeyCode.Alpha2))
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+      if (!rotationMat.IsEmpty && !translationVector.IsEmpty)
+      {
+        Emgu.CV.Mat r3 = new Emgu.CV.Mat();
+        Emgu.CV.CvInvoke.Rodrigues(rotationMat, r3);
 
-        if (Input.GetKey(KeyCode.Alpha3))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        print("r");
+        print(ARKit.MatExtension.GetValue(rotationMat, 0, 0));
+        print(ARKit.MatExtension.GetValue(rotationMat, 0, 1));
+        print(ARKit.MatExtension.GetValue(rotationMat, 0, 2));
+        print(ARKit.MatExtension.GetValue(rotationMat, 1, 0));
+        print(ARKit.MatExtension.GetValue(rotationMat, 1, 1));
+        print(ARKit.MatExtension.GetValue(rotationMat, 1, 2));
+        print(ARKit.MatExtension.GetValue(rotationMat, 2, 0));
+        print(ARKit.MatExtension.GetValue(rotationMat, 2, 1));
+        print(ARKit.MatExtension.GetValue(rotationMat, 2, 2));
+        print("t");
+        print(ARKit.MatExtension.GetValue(translationVector, 0, 0));
+        print(ARKit.MatExtension.GetValue(translationVector, 0, 1));
+        print(ARKit.MatExtension.GetValue(translationVector, 0, 2));
 
-        if (Input.GetKey(KeyCode.Alpha4))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        float[] eulers = WebCamera.fp.GetEulerAngles(r3);
 
-        if (Input.GetKey(KeyCode.Alpha5))
-            transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+        Vector3 angles = new Vector3(eulers[0], eulers[1], eulers[2]);
 
-        if (Input.GetKey(KeyCode.Alpha6))
-            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.Alpha7))
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.Alpha8))
-            transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
+        transform.Rotate(angles);
+      }
     }
+  }
+
 }
 
--- EDP Unity/Assets/Scripts/WebCamera.cs
+++ EDP Unity/Assets/Scripts/WebCamera.cs
@@ -5,61 +5,140 @@ using UnityEngine.UI;
 
 public class WebCamera : MonoBehaviour
 {
+    private Texture2D backgroundTexture;
+    private ARKit.Camera capture;
+    static public ARKit.FeaturePoints fp = null;
+    static public ARKit.InitialFrame ip = null;
+    public bool initialMatchDone = false;
 
-
+    /*
     private bool camAvailable;
     private WebCamTexture webCam;
     private Texture defaultBackground;
+    */
 
     public RawImage background;
     public AspectRatioFitter fit;
+    public Canvas canvas;
 
     private void Start()
     {
-        defaultBackground = background.texture;
-        WebCamDevice[] devices = WebCamTexture.devices;
+        // defaultBackground = background.texture;
+        // WebCamDevice[] devices = WebCamTexture.devices;
 
-        if (devices.Length == 0)
-        {
-            Debug.Log("No camera detected");
-            camAvailable = false;
-            return;
-        }
-
-        for (int i = 0; i < devices.Length; i++)
-        {
-            if (devices[i].isFrontFacing) {
-                webCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
-            }
-        }
-
-        if(webCam == null)
-        {
-            Debug.Log("Unable to find camera");
-            return;
-        }
-
-        webCam.Play();
-        background.texture = webCam;
-
-        camAvailable = true;
-
-    }
-
-
-    private void Update()
+    /*
+    if (devices.Length == 0)
     {
-        if (!camAvailable)
-            return;
-
-        float ratio = (float)webCam.width / (float)webCam.height;
-
-        fit.aspectRatio = ratio;
-
-        float scaleY = webCam.videoVerticallyMirrored ? -1f : 1f;
-        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
-
-        int orient = -webCam.videoRotationAngle;
-        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        Debug.Log("No camera detected");
+        camAvailable = false;
+        return;
     }
+
+    for (int i = 0; i < devices.Length; i++)
+    {
+        if (devices[i].isFrontFacing) {
+            webCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+        }
+    }
+
+    if(webCam == null)
+    {
+        Debug.Log("Unable to find camera");
+        return;
+    }
+
+    webCam.Play();
+    background.texture = webCam;
+
+    camAvailable = true;
+    */
+
+    ARKit.Frame frame;
+
+    this.capture = new ARKit.Camera(1, new ARKit.Size(1080, 720));
+
+    frame = this.capture.GetNextFrame();
+
+    this.backgroundTexture = new Texture2D(frame.Width, frame.Height);
+    background.texture = this.backgroundTexture; // set texture to webcam frames
+    // referenced from https://answers.unity.com/questions/23891/resizing-an-object.html
+    canvas.transform.localScale = new Vector3((float)frame.Width / (float)frame.Height, 1, 1); // fix resolution of plane
+
+    if (System.IO.File.Exists("intrinsics.yml"))
+    {
+        ip = new ARKit.InitialFrame();
+        ip.ReadFromFile();
+    }
+    else
+    {
+        ip = new ARKit.InitialFrame(this.capture, new ARKit.Size(4, 7), 30);
+        ip.Start();
+    }
+
+    ARKit.FeaturePoints.ComputeAndSave("agenda.jpg", "Assets/keypoints.yml");
+    fp = ARKit.FeaturePoints.ReadData("Assets/keypoints.yml", ip.Homography);
+  }
+
+
+  private void Update()
+  {
+    /*
+    if (!camAvailable)
+        return;
+
+    float ratio = (float)webCam.width / (float)webCam.height;
+
+    fit.aspectRatio = ratio;
+
+    float scaleY = webCam.videoVerticallyMirrored ? -1f : 1f;
+    background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+    int orient = -webCam.videoRotationAngle;
+    background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    */
+
+    bool tracking = false;
+
+    if (ip != null && fp != null)
+    {
+      if (!ip.Homography.IsEmpty)
+      {
+        var frame = this.capture.GetNextFrame();
+
+        if (!initialMatchDone)
+        {
+          fp.ComputeAndMatch();
+          initialMatchDone = true;
+        }
+        else
+          tracking = fp.TrackObject();
+
+        if (fp.FindObject(!tracking))
+        {
+          if (fp.GetPose(ip.CameraMatrix, ip.DistortionCoefficients, out Emgu.CV.Mat rotations, out Emgu.CV.Mat translations))
+          {
+            frame = fp.DrawObjectBorder(true, ip.CameraMatrix, ip.DistortionCoefficients, rotations, translations);
+          }
+          else
+            frame = fp.DrawObjectBorder();
+
+          print("object found");
+        }
+        else
+          print("object not found");
+
+        this.backgroundTexture.LoadImage(frame.Image);
+
+
+
+        // Camera cam = Camera.main;
+        /*if (this.fp.GetProjectionMatrix(out Matrix4x4 p))
+        {
+          cam.projectionMatrix = p;
+
+          print("projection matrix set");
+        }*/
+      }
+    }
+  }
 }
